cf894e46baac9429e3b1665ad9ae8fface066d8b
Alexander Ignatov <ignalex@gmail.com>
2017-08-26 12:51:33 +1000

    init

--- modules/blink.py
+++ modules/blink.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """
+test bliking simple 
+
 Created on Tue Apr 08 12:06:44 2014
 
 @author: aignatov
@@ -7,36 +9,35 @@ v1. - triggers basic
 v2. - no start 
 """
 import sys
-try: 
-	sys.path.append('/storage/PY_modules/')
-except: 
-	pass 
-
-if len(sys.argv)>1: 
-    B, N,mult = int(sys.argv[1]), int(sys.argv[2]),  float(sys.argv[3])
-else: 
-    B, N,mult = 3, 3, 0.1 
-
 from time import sleep 
- 
-# -------------------------------------------------------------------------    
-channel = 18
 
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 
+def blink(pin=18, B=3, N=3, mult=0.1):
+    GPIO.setup(pin , GPIO.OUT, initial = GPIO.HIGH)
 
-GPIO.setup(channel , GPIO.OUT, initial = GPIO.HIGH)
+    for b in range(B):      
+        for a in range(N):
+            GPIO.output(pin, GPIO.LOW)	
+            sleep(mult)
+            GPIO.output(pin, GPIO.HIGH)
+            sleep(mult)
+        sleep(0.3)
+    GPIO.cleanup(pin)# - GPIO to be cleaned up in main module only 
 
-for b in range(B):      
-    for a in range(N):
-        # -------------- START GPIO ------------------- 
-        GPIO.output(channel, GPIO.LOW)	
-        sleep(mult)
-        GPIO.output(channel, GPIO.HIGH)
-        sleep(mult)
-    sleep(0.3)
+#%%
+if __name__ == '__main__': 
+  
+  if len(sys.argv)>1: 
+      pin, B, N, mult = int(sys.argv[1]), int(sys.argv[2]),int(sys.argv[3]), float(sys.argv[4])
+  else: 
+      pin, B, N, mult = 18, 3, 3, 0.1 
+
+  blink(pin, B, N, mult)
 
 
-GPIO.cleanup(channel)# - GPIO to be cleaned up in main module only 
+
+
+
 

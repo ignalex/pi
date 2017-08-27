f49b1ab3fce40833456fab7ec1d32bef749e9e2c
Alexander Ignatov <ignalex@gmail.com>
2017-08-28 07:49:10 +1000

    fix

--- modules/pa.py
+++ modules/pa.py
@@ -14,7 +14,7 @@ def pa(arg):
     if type(arg) == list: arg = arg[0]
 #    path = [os.path.join(i, 'PA.py ') for i in [j for j in ['/home/pi/PYTHON/GPIO','/home/pi/git/pi'] if os.path.exists(j)][0]][0]
 #    os.system("sudo python " + path + arg + " &" )
-    os.system("ssh -p 2227 -i home/pi/.ssh/octopus pi@192.168.1.154 nohup sudo python /home/pi/PYTHON/GPIO/PA.py '\"" + arg+ "\"' &" )
+    os.system("ssh -p 2227 -i /home/pi/.ssh/octopus pi@192.168.1.154 nohup sudo python /home/pi/PYTHON/GPIO/PA.py '\"" + arg+ "\"' &" )
 
 if __name__ == '__main__': 
     arg = sys.argv[1]

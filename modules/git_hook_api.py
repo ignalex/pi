74568b3e6110e1984f3c8d0b552506c040df4d44
Alexander Ignatov <ignalex@gmail.com>
2017-09-01 08:18:57 +1000

    git hooks > security

--- modules/git_hook_api.py
+++ modules/git_hook_api.py
@@ -28,15 +28,23 @@ app = Flask(__name__)
 @app.route("/git_pull", methods=['POST'])
 def git_pull():
     global logger, p
+    j = request.json # payload
     with open(os.path.join(Dirs()['LOG'],'git_payload_{}.pcl'.format(str(datetime.datetime.now()))), 'wb') as f: 
-        pickle.dump(request.json, f)
+        pickle.dump(j, f)
+    
+    #checking payload 
+    if j['head_commit']['committer']['email'] != p.GIT_CI.check_committer: 
+        Speak('commit security check not passed')
+        logger.error('commit security check not passed')
+        return 'commit security check not passed'
+    
     process = Popen('git pull'.split(' '), stdout=PIPE, stderr=PIPE, cwd=Dirs()['REPO'])
     reply  = ' : '.join([str(i) for i in process.communicate() ]) 
     logger.info(reply)
     if p.GIT_CI.report_pull_email == 'YES' or p.GIT_CI.report_pull_email == True: 
         logger.info(sendMail([p.email.address], [p.email.address, p.email.login, p.email.password], 'CI - git pull from ' + socket.gethostname(), reply ,[]))
     if p.GIT_CI.report_speak == 'YES' or p.GIT_CI.report_speak == True: 
-        Speak('git pull done')
+        Speak('git pull with the message {} done on the machine {}'.format(', '.join([i['message'] for i in j['commits']]), socket.gethostname()))
     return reply 
 
 if __name__ == '__main__' : 

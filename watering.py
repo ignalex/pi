44d71a07fb0a8c2e5b8dae466bc245d6a44b441f
Alexander Ignatov <ignalex@gmail.com>
2017-10-26 06:41:10 +1100

    second set for watering in high temperatures

--- watering.py
+++ watering.py
@@ -8,9 +8,15 @@ from modules.speak_over_ssh import Speak
 from modules.weather import WEATHER
 from modules.common import LOGGER, CONFIGURATION
 
-def extra_for_weather(threshhold=20, extra=2, was_raining=5): 
+def extra_for_weather(threshhold=20, extra=2, was_raining=5,min_temp_set=0): 
     w = WEATHER(False)
     w.ToInt()
+
+    # if min_temp_set > 0 - checking if actual temp > min_temp_set and watering only if yes
+    if min_temp_set > 0 and w.temp_today <= min_temp_set: 
+        Speak('it is too cold, no extra watering' )
+        logger.info('temperature forecasted ' + str(w.temp_today) + ' less than ' + str(min_temp_set))
+        sys.exit()
 
     if w.WasRaining(was_raining): 
         Speak('it was raining for '+ str(w.rain) + ' mm yesterday. I will not water the plants' )
@@ -33,10 +39,9 @@ def water(params):
 if __name__ == '__main__': 
     logger = LOGGER('watering','INFO', True)
     p = CONFIGURATION()
+    min_temp_set = [sys.argv[1] if len(sys.argv)>1 else 'min_temp_set1'][0]
     
-    extra_time = extra_for_weather(p.water.extra_threshold, p.water.extra,p.water.was_raining)
-#    params = {1 : {'relay': 2, 'motor' : 1, 'time' : 20  + extra_time}, 
-#              2 : {'relay': 3, 'motor' : 1, 'time' : 30  + extra_time}} 
+    extra_time = extra_for_weather(p.water.extra_threshold, p.water.extra,p.water.was_raining, int(getattr(p.water,min_temp_set)))
 
     params = {1 : {'relay': p.pins.water1, 'motor' : p.pins.motor, 'time' : p.water.time1  + extra_time}, 
               2 : {'relay': p.pins.water2, 'motor' : p.pins.motor, 'time' : p.water.time2  + extra_time}} 

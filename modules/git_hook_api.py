3f13d119aedce604139eacc0c747f6a4d159ff9e
Alexander Ignatov <ignalex@gmail.com>
2018-02-22 06:56:03 +1100

    git hook udapte

--- modules/git_hook_api.py
+++ modules/git_hook_api.py
@@ -31,26 +31,30 @@ app = Flask(__name__)
 
 @app.route("/git_pull", methods=['POST'])
 def git_pull():
-    global logger, p
-    j = request.json # payload
-    if p.GIT_CI.save_payload:
-        with open(os.path.join(Dirs()['LOG'],'git_payload_{}.pcl'.format(str(datetime.datetime.now()))), 'wb') as f:
-            pickle.dump(j, f)
+    try:
+        global logger, p
+        j = request.json # payload
+        if p.GIT_CI.save_payload:
+            with open(os.path.join(Dirs()['LOG'],'git_payload_{}.pcl'.format(str(datetime.datetime.now()))), 'wb') as f:
+                pickle.dump(j, f)
 
-    #checking payload
-    if j['head_commit']['committer']['email'] != p.GIT_CI.check_committer:
-        Speak('commit security check not passed')
-        logger.error('commit security check not passed')
-        return 'commit security check not passed'
+        #checking payload
+        if j['head_commit']['committer']['email'] != p.GIT_CI.check_committer:
+            Speak('commit security check not passed')
+            logger.error('commit security check not passed')
+            return 'commit security check not passed'
 
-    process = Popen('git pull'.split(' '), stdout=PIPE, stderr=PIPE, cwd=Dirs()['REPO'])
-    reply  = ' : '.join([str(i) for i in process.communicate() ])
-    logger.info(reply)
-    if p.GIT_CI.report_pull_email:
-        logger.info(sendMail([p.email.address], [p.email.address, p.email.login, p.email.password], 'CI - git pull from ' + socket.gethostname(), reply ,[]))
-    if p.GIT_CI.report_speak:
-        Speak('git pull with the message {} done on the machine {}'.format(', '.join([i['message'] for i in j['commits']]), socket.gethostname()))
-    return reply
+        process = Popen('git pull'.split(' '), stdout=PIPE, stderr=PIPE, cwd=Dirs()['REPO'])
+        reply  = ' : '.join([str(i) for i in process.communicate() ])
+        logger.info(reply)
+        if p.GIT_CI.report_pull_email:
+            logger.info(sendMail([p.email.address], [p.email.address, p.email.login, p.email.password], 'CI - git pull from ' + socket.gethostname(), reply ,[]))
+        if p.GIT_CI.report_speak:
+            Speak('git pull with the message {} done on the machine {}'.format(', '.join([i['message'] for i in j['commits']]), socket.gethostname()))
+        return reply
+    except Exception as e:
+        logger.error(e)
+        return 'error ' + str(e)
 
 if __name__ == '__main__' :
     with daemon.DaemonContext(files_preserve = [logger.handlers[0].stream,]): # preserving logger for daemon
--- modules/send_email.py
+++ modules/send_email.py
@@ -1,28 +1,30 @@
+from __future__ import print_function
+
 import os, smtplib
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from email.Utils import COMMASPACE, formatdate 
+from email.Utils import COMMASPACE, formatdate
 from email import encoders
 
-def sendMail(to, from_credentials, subject, text, files): 
-    try: 
-        assert type(to)==list 
-        assert type(files)==list 
-        
+def sendMail(to, from_credentials, subject, text, files):
+    try:
+        assert type(to)==list
+        assert type(files)==list
+
         from_addr = from_credentials[0]
-                
-        connection = smtplib.SMTP_SSL('smtp.gmail.com') 
-        connection.login(from_credentials[1],from_credentials[2])    
-       
+
+        connection = smtplib.SMTP_SSL('smtp.gmail.com')
+        connection.login(from_credentials[1],from_credentials[2])
+
         msg = MIMEMultipart()
         msg['From'] = from_addr
         msg['To'] = COMMASPACE.join(to)
         msg['Date'] = formatdate(localtime=True)
         msg['Subject'] = subject
-        
+
         msg.attach(MIMEText(text))
-        
+
         for file_ in files:
             part = MIMEBase('application', "octet-stream")
             part.set_payload( open(file_,"rb").read() )
@@ -30,34 +32,34 @@ def sendMail(to, from_credentials, subject, text, files):
             part.add_header('Content-Disposition', 'attachment; filename="%s"'
                            % os.path.basename(file_))
             msg.attach(part)
-           
-        connection.sendmail(from_addr, msg['To'] , msg.as_string() )      
+
+        connection.sendmail(from_addr, msg['To'] , msg.as_string() )
         connection.close()
         return 'sent'
-    except Exception, exc:
+    except Exception as exc:
         return "ERROR SENDING E-MAIL: "+ str(exc)
 
 def MyIP():
     from requests import get
     return get('https://api.ipify.org').text
 
-if __name__ == '__main__': 
-    import sys 
-    sys.path.append('/home/pi/git/pi/') #for crontab 
+if __name__ == '__main__':
+    import sys
+    sys.path.append('/home/pi/git/pi/') #for crontab
     from modules.common import CONFIGURATION, LOGGER, LastLine
     p = CONFIGURATION()
-    if len(sys.argv)> 1: 
+    if len(sys.argv)> 1:
         if sys.argv[1] == 'ip' :
             "syntax : python send_email.py ip {to} {from} {login} {pass} "
             logger = LOGGER('send_ip', 'WARNING')
-            try: 
+            try:
                 if sys.argv[1] == 'ip' :
                     _ip, _to, _from_addr, _from_login, _from_pass = sys.argv[1:]
                     IP = MyIP()
-                    if LastLine('/home/pi/LOG/send_ip', 2).find(IP) == -1: # updated 
-                        reply = sendMail([_to], [_from_addr, _from_login, _from_pass], 'IP', IP ,[]) +  ' IP changed' 
-                    else: 
+                    if LastLine('/home/pi/LOG/send_ip', 2).find(IP) == -1: # updated
+                        reply = sendMail([_to], [_from_addr, _from_login, _from_pass], 'IP', IP ,[]) +  ' IP changed'
+                    else:
                         reply = 'IP same '
                 logger.warn('OK, IP =' + IP + ' ' + reply)
-            except: 
+            except:
                 logger.error('NOT GOOD. IP =' + IP + ' ' + reply)

a0f899e5aef6d82d882124a6faefeb4ffad2ff02
Alexander Ignatov <ignalex@gmail.com>
2018-05-02 09:47:00 +1000

    MS

--- MS.py
+++ MS.py
@@ -114,8 +114,8 @@ GPIO.ON = GPIO.LOW
 GPIO.OFF = GPIO.HIGH
 
 GPIO.setup(p.pins.BLINK, GPIO.OUT, initial = GPIO.INIT)
-GPIO.setup(p.pins.MOVEMENT_SENSOR, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #TODO: to try PUD_UP PUD DOWN
-#GPIO.setup(p.pins.MOVEMENT_SENSOR, GPIO.IN, pull_up_down=GPIO.PUD_UP) #TODO: to try PUD_UP PUD DOWN
+#GPIO.setup(p.pins.MOVEMENT_SENSOR, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #TODO: to try PUD_UP PUD DOWN
+GPIO.setup(p.pins.MOVEMENT_SENSOR, GPIO.IN, pull_up_down=GPIO.PUD_UP) #TODO: to try PUD_UP PUD DOWN
 
 #%% extra modules
 def Blink(args = [1,1,0.1]):

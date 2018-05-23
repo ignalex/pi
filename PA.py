23fd1b4cfa859cdd09f2aba80dad54afefcec335
Alexander Ignatov <ignalex@gmail.com>
2018-05-23 20:39:35 +1000

    fix

--- PA.py
+++ PA.py
@@ -25,7 +25,7 @@ except:
 #from heater import TranslateForHornet as HEATER
 #from modules.slang import SLANG
 from modules.control_esp import ESP as esp  #control_esp as ESP
-from modules.PingIPone import AcquireResult
+from modules.PingIPhone import AcquireResult
 
 #class PARAMETERS (object):
 #    def __init__(self, ID = ''):

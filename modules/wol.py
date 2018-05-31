08c1cfe810ea98fd29911a53931c76b47318d450
Alexander Ignatov <ignalex@gmail.com>
2018-06-01 08:27:27 +1000

    wol for surface

--- modules/wol.py
+++ modules/wol.py
@@ -3,10 +3,11 @@ from __future__ import print_function
 import struct, socket, sys 
 
 #TODO: something is broken...
-def wol(PC = ['ZHIRAFFA']):
+def wol(PC = ['SURFACE']):
     PCs = {'ZHIRAFFA': '2C:27:D7:19:70:CF', 
-           'KROT' : '00:b3:f6:03:09:85', 
-           'MONSTER' : '1c:6f:65:d5:3c:b6'}
+           #'KROT' : '00:b3:f6:03:09:85', 
+           'MONSTER' : '1c:6f:65:d5:3c:b6', 
+           'SURFACE' : '58:82:a8:94:82:02'}
 
     ethernet_address = PCs[PC[0]]
   # Construct a six-byte hardware address

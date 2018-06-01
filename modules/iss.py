9b8d1226b5d572a24ad318a0d6c208c868ab94c7
Alexander Ignatov <ignalex@gmail.com>
2018-06-02 09:34:00 +1000

    iss simplification

--- modules/iss.py
+++ modules/iss.py
@@ -3,11 +3,21 @@
 Created on Mon Dec 07 06:48:31 2015
 
 @author: Alex
+
+
+    #DONE: logger + data > to DB 
+    #MAYBE: distance calc from DB 
+    #DONE: indexes to DB + geom + make table instead of pandas
+    #DONE: silence during night + when speaking > still scanning 
+    #TODO: when approaching > reduce delay 
+    #TODO: alert only once per approach > accumulate and send once 
+    #DONE: class initiate only once
+    
 """
 from __future__ import print_function
 import requests, datetime, os, time , math 
 
-# disabling warning message
+# disabling warning message # somehow still goes on Pi
 import requests.packages.urllib3
 requests.packages.urllib3.disable_warnings()
 
@@ -29,20 +39,18 @@ class GET_API(object):
 class ISS(GET_API): 
     """ISS postition and people class v 2 - realtime """
     def __init__(self): 
-        GET_API.__init__(self,url = "https://api.wheretheiss.at/v1/satellites/25544", params = None, name = 'iss',proxies = proxies)    
         self.keys = ['timestamp','latitude','longitude','velocity','altitude']
+    def Scan(self): 
+        GET_API.__init__(self,url = "https://api.wheretheiss.at/v1/satellites/25544", params = None, name = 'iss',proxies = proxies)    
         for k in self.keys: setattr(self,k,self.iss[k])
         self.timestamp = datetime.datetime.fromtimestamp(self.timestamp)
-        self.HowFar()
-    def HowFar(self): 
-        self.how_far = int(Distance(location,(( self.longitude,self.latitude)))/1000)
+        self.how_far = int(Distance(location,(( self.longitude,self.latitude)))/1000)       
     def Log(self):       
         print ('\t'.join([str(getattr(self,i)) for i in self.keys + ['how_far']]) )
         #to DB 
         sql = "insert into iss_position (timestamp,latitude,longitude,velocity,altitude,how_far) values ('{}',{},{},{},{},{})".\
             format(self.timestamp, self.latitude, self.longitude, self.velocity, self.altitude, self.how_far)
         cur.execute(sql)                                                        
-        
         if alert: 
             Alert(self.how_far)
 
@@ -50,7 +58,6 @@ def Distance(origin, destination):
     lon1, lat1 = [float(i) for i in origin]
     lon2, lat2 = [float(i) for i in destination]
     radius = 6371000 # km
-
     dlat, dlon = math.radians(lat2-lat1), math.radians(lon2-lon1)
     a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
         * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
@@ -61,34 +68,28 @@ def Distance(origin, destination):
 def Alert(distance): 
     global alert_time, iss 
     if distance <= alert_distance: 
-        if (datetime.datetime.now() - alert_time).seconds > 5 * 60 : #and \
-            if alert_type == 'speak': 
-                os.system('python /home/pi/git/pi/modules/talk.py "international space station approaching, distance is {0} kilometers"'.format(str(distance)))
-            elif alert_type == 'print': 
-                print ('international space station approaching, distance is {0}'.format(str(distance)))
-            alert_time = datetime.datetime.now()
-        
+        if (datetime.datetime.now() - alert_time).seconds > 5 * 60 :  
+            alert_time = datetime.datetime.now()            
+            if alert_type == 'speak' and alert_time.hour > 7 and alert_time.hour < 22: 
+                os.system('python /home/pi/git/pi/modules/talk.py "international space station approaching, distance is {0} kilometers" &'.format(str(distance)))
+
         alert_message = '\t'.join([str(i) for i in [str(datetime.datetime.now()).split('.')[0], distance, iss.latitude, iss.longitude]]) 
         print (alert_message, open(os.path.join(Dirs()['LOG'], 'iss_alert'),'a'))
-        
         logger.info(alert_message)
         logger.info('sending email ... ' + sendMail([p.email.address], [p.email.address, p.email.login, p.email.password], 'ISS approaching alert ', alert_message ,[]))
 
 def Start(): 
     global iss, delay
     last_time_stamp = None
+    iss = ISS() 
     while True: 
-        iss = ISS()
+        iss.Scan()
         if iss.timestamp != last_time_stamp: iss.Log()
         last_time_stamp =  iss.timestamp    
         time.sleep(delay)
          
 if __name__ == '__main__': 
 
-    #DONE: logger + data > to DB 
-    #TODO: distance calc from DB 
-    #DONE: indexes to DB + geom + make table instead of pandas
-    
     logger = LOGGER('iss', 'INFO')
     p = CONFIGURATION()
     

b71d4a342763d3d20d08081b5d014a572e551d94
Alexander Ignatov <ignalex@gmail.com>
2018-11-13 10:32:11 +1100

    esp status collector

--- hap_server.py
+++ hap_server.py
@@ -16,9 +16,9 @@ from pyhap.accessory_driver import AccessoryDriver
 # The below package can be found in the HAP-python github repo under accessories/
 
 from accessories_ai.sensors import TemperatureSensor, LightSensor#, InternetSpeed
-from accessories_ai.switches import AllSwitches, ProgramableSwitch
+from accessories_ai.switches import AllSwitches, ProgramableSwitch, EspStatusCollector
 from accessories_ai.windows import WindowCovering
-from accessories_ai.computers import SYSTEM, EspStatusCollector
+from accessories_ai.computers import SYSTEM
 
 from common import LOGGER
 logger = LOGGER('HAP_server', 'DEBUG')

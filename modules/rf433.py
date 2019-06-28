1eb2d7d504e0b3ce4817dccdceaa122a131d7639
Alexander Ignatov <ignalex@gmail.com>
2019-06-28 17:07:13 +1000

    rf433

---
+++ modules/rf433.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 28 09:07:04 2019
+
+@author: alexander
+"""
+
+# converter pinout 
+# https://learn.sparkfun.com/tutorials/retired---using-the-logic-level-converter
+
+# PI pinout 
+# https://learn.sparkfun.com/tutorials/raspberry-gpio/all
+
+from __future__ import print_function
+from time import  sleep 
+
+RF433 = 16 # pin 16 (board) = GPIO23
+
+#%% GPIO
+import RPi.GPIO as GPIO # if there is error on import not found RPI - need to enable device tree from raspi-config
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.INIT = GPIO.HIGH
+GPIO.ON = GPIO.LOW
+GPIO.OFF = GPIO.HIGH
+
+GPIO.setup(RF433, GPIO.OUT, initial = GPIO.INIT)
+
+#%%
+
+RF_positions = {} #global > will be updated on 1st request
+timeDelay = 200 / 1000000 #float(value[0])/1000000
+
+
+def rf433(value):
+    """wrapper for _rf433
+    [group,what]
+    returns [{type:[status::bool, message::str}]
+    """
+    global RF_positions
+    print ('rf warapper: value: ' + str(value))
+    com = [1 if value[1] in [1, '1','on','ON','True','true'] else 0][0]
+    if value[0] == 'light':
+        res = [_rf433([5,com]) ]
+    elif value[0] == 'heater':
+        res = [_rf433([12,com])]
+    elif value[0] == 'coffee':
+        res = [_rf433([11,com])]
+    elif value[0] == 'all':
+        res = [_rf433([v,com]) for v in [5,11,12,21]]
+    else:
+        res = [_rf433([value[0],com])]
+    RF_positions[value[0]] = com
+#    ret= str(value[0]) +'<br>' + '<br>'.join(res)
+    return [RF_positions, res] # updating current state > will be [1/0,'string message']
+
+def _rf433(value):
+    """[signal, OnOff]
+    : 150 - 300 delay good, pin 14 (D5), 5V """
+    print ('_rf433' + str(value))
+    signal = value[0]
+    OnOff  = int(value[1])
+    signals= {'1' : [
+                  [2,4,2,4,2,2,2,2,4,2,4,2,4,4,2,4,2,2,2,2,2,4,4,4,2,2,2,2,2,4,4,2,3],
+                  [2,4,2,4,2,2,2,2,4,2,4,2,4,4,2,4,2,2,2,2,4,4,4,4,2,2,2,2,4,2,4,2,3]
+                  ],
+              '2':  [
+                  [2,4,2,4,2,2,2,2,4,2,4,2,4,4,2,4,2,2,2,2,2,2,4,4,2,2,2,2,2,2,2,4,3],
+                  [2,4,2,4,2,2,2,2,4,2,4,2,4,4,2,4,2,2,2,2,4,2,4,4,2,2,2,2,4,4,4,2,3]
+                  ],
+              '3':  [
+                  [2,4,2,4,2,2,2,2,4,2,4,2,4,4,2,4,2,2,2,2,2,4,2,4,2,2,2,2,2,4,2,4,3],
+                  [2,4,2,4,2,2,2,2,4,2,4,2,4,4,2,4,2,2,2,2,4,4,2,4,2,2,2,2,4,2,2,4,3]
+                  ],
+              '4':  [
+                  [2,4,2,4,2,2,2,2,4,2,4,2,4,4,2,4,2,2,2,2,2,4,4,2,2,2,2,2,2,4,4,4,3],
+                  [2,4,2,4,2,2,2,2,4,2,4,2,4,4,2,4,2,2,2,2,4,4,4,2,2,2,2,2,4,2,4,4,3]
+                  ],
+              # for all 1-4
+              '5':  [
+                  [2,4,2,4,2,2,2,2,4,2,4,2,4,4,2,4,2,2,2,2,4,2,2,2,2,2,2,2,4,4,2,2,3],
+                  [2,4,2,4,2,2,2,2,4,2,4,2,4,4,2,4,2,2,2,2,2,4,2,2,2,2,2,2,2,4,2,2,3]
+                  ],
+              '11': [
+                  [4,2,2,4,4,2,4,2,4,4,4,2,2,2,2,2,2,2,2,2,4,4,4,2,2,2,2,2,4,2,4,4,2,3],
+                  [4,2,2,4,4,2,4,2,4,4,4,2,2,2,2,2,2,2,2,2,4,4,4,4,2,2,2,2,4,2,4,2,2,3]
+                  ],
+              '12': [
+                  [4,2,2,4,4,2,4,2,4,4,4,2,2,2,2,2,2,2,2,2,4,4,2,2,2,2,2,2,4,2,2,4,2,3],
+                  [4,2,2,4,4,2,4,2,4,4,4,2,2,2,2,2,2,2,2,2,4,4,2,4,2,2,2,2,4,2,2,2,2,3]
+                  ],
+              '13': [
+                  [4,2,2,4,4,2,4,2,4,4,4,2,2,2,2,2,2,2,2,2,4,2,4,2,2,2,2,2,4,4,2,4,2,3],
+                  [4,2,2,4,4,2,4,2,4,4,4,2,2,2,2,2,2,2,2,2,4,2,4,4,2,2,2,2,4,4,2,2,2,3]
+                  ],
+              '14': [
+                  [4,2,2,4,4,2,4,2,4,4,4,2,2,2,2,2,2,2,2,2,2,4,4,2,2,2,2,2,2,2,4,4,2,3],
+                  [4,2,2,4,4,2,4,2,4,4,4,2,2,2,2,2,2,2,2,2,2,4,4,4,2,2,2,2,2,2,4,2,2,3]
+                  ],
+              '99': [ # all 4
+                  [4,2,2,4,4,2,4,2,4,4,4,2,2,2,2,2,2,2,2,2,4,2,2,2,2,2,2,2,4,4,4,2,2,3],
+                  [4,2,2,4,4,2,4,2,4,4,4,2,2,2,2,2,2,2,2,2,2,4,2,2,2,2,2,2,2,2,2,4,2,3]
+                  ],
+              # another module > not used.
+              '21': [
+                  [1,4,1,4,1,4,1,4,1,1,1,1,1,1,1,1,1,1,1,4,4,4,1,1,4,1,1,4,1,1,4,4,4,4,4,1,1,1,1,4,4,4,4,4,4,4,4,6],
+                  [1,4,1,4,1,4,1,4,1,1,1,1,1,1,1,1,1,1,1,4,4,4,1,1,4,1,1,4,1,1,4,4,4,4,4,1,1,1,1,4,4,4,4,4,4,4,4,6]
+                  ]
+              }
+    mapping = {1 : (3,3), 2 : (3,7), 3: (3,92), 4: (7,3), 5 : (7,7), 6 : (7,92)}
+    
+#    p = Pin(pin , Pin.OUT)
+    GPIO.setup(RF433, GPIO.OUT, initial = GPIO.INIT)
+
+
+    try:
+        for n in range(0,8):
+            for i in signals[str(signal)][OnOff]:
+#                p.high()
+                GPIO.output(RF433, GPIO.HIGH)
+                #led.high()
+                sleep((mapping[i][0] * timeDelay))
+#                p.low()
+                GPIO.output(RF433, GPIO.LOW)
+                #led.low()
+                sleep((mapping[i][1] * timeDelay))
+        #led.high()
+        sleep(0.2)
+        return str('signal {} sent to {}'.format(OnOff,signal))
+    except Exception as e:
+        print (str(e))
+        return str(e)

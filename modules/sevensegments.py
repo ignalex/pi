436cf16aef0e6006152ca78f7cace2fbf8dcffb5
Alexander Ignatov <ignalex@gmail.com>
2019-07-03 11:46:15 +1000

    sevensegments

--- modules/sevensegments.py
+++ modules/sevensegments.py
@@ -1,67 +1,153 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jul  1 14:15:31 2019
-
-SPI
- https://www.raspberrypi-spy.co.uk/2016/03/7-segment-display-modules-and-the-raspberry-pi/
- sudo apt-get install libjpeg-dev
- sudo pip3 install pillow==4.0.0
- from max7219 sudo python3 setup.py install
+Created on Wed Jul  3 10:59:11 2019
 
 @author: alexander
 """
-import time, sys
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017-18 Richard Hull and contributors
+# See LICENSE.rst for details.
+
+"""
+Example for seven segment displays.
+"""
+
+import time
 from datetime import datetime
 
-import max7219.led as led
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.virtual import viewport, sevensegment
 
-class sevensegments(object): 
-    def __init__ (self, deviceId=0 ): 
-        self.device = led.sevensegment()
-        self.deviceId = deviceId
+
+def date(seg):
+    """
+    Display current date on device.
+    """
+    now = datetime.now()
+    seg.text = now.strftime("%y-%m-%d")
+
+
+def clock(seg, seconds):
+    """
+    Display current time on device.
+    """
+    interval = 0.5
+    for i in range(int(seconds / interval)):
+        now = datetime.now()
+        seg.text = now.strftime("%H-%M-%S")
+
+        # calculate blinking dot
+        if i % 2 == 0:
+            seg.text = now.strftime("%H-%M-%S")
+        else:
+            seg.text = now.strftime("%H %M %S")
+
+        time.sleep(interval)
+
+
+def show_message_vp(device, msg, delay=0.1):
+    # Implemented with virtual viewport
+    width = device.width
+    padding = " " * width
+    msg = padding + msg + padding
+    n = len(msg)
+
+    virtual = viewport(device, width=n, height=8)
+    sevensegment(virtual).text = msg
+    for i in reversed(list(range(n - width))):
+        virtual.set_position((i, 0))
+        time.sleep(delay)
+
+
+def show_message_alt(seg, msg, delay=0.1):
+    # Does same as above but does string slicing itself
+    width = seg.device.width
+    padding = " " * width
+    msg = padding + msg + padding
+
+    for i in range(len(msg)):
+        seg.text = msg[i:i + width]
+        time.sleep(delay)
+
+
+def main():
+    # create seven segment device
+    serial = spi(port=0, device=0, gpio=noop())
+    device = max7219(serial, cascaded=1)
+    seg = sevensegment(device)
+
+    print('Simple text...')
+    for _ in range(8):
+        seg.text = "HELLO"
+        time.sleep(0.6)
+        seg.text = " GOODBYE"
+        time.sleep(0.6)
+
+    # Digit slicing
+    print("Digit slicing")
+    seg.text = "_" * seg.device.width
+    time.sleep(1.0)
+
+    for i, ch in enumerate([9, 8, 7, 6, 5, 4, 3, 2]):
+        seg.text[i] = str(ch)
+        time.sleep(0.6)
+
+    for i in range(len(seg.text)):
+        del seg.text[0]
+        time.sleep(0.6)
+
+    # Scrolling Alphabet Text
+    print('Scrolling alphabet text...')
+    show_message_vp(device, "HELLO EVERYONE!")
+    show_message_vp(device, "PI is 3.14159 ... ")
+    show_message_vp(device, "IP is 127.0.0.1 ... ")
+    show_message_alt(seg, "0123456789 abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+    # Digit futzing
+    date(seg)
+    time.sleep(5)
+    clock(seg, seconds=10)
+
+    # Brightness
+    print('Brightness...')
+    for x in range(5):
+        for intensity in range(16):
+            seg.device.contrast(intensity * 16)
+            time.sleep(0.1)
+    device.contrast(0x7F)
+
+#%% abstract to one object
+    
+class SevenSegments(object): 
+    def __init__ (self, port=0, device=0 ): 
+        self.serial = spi(port=port, device=device, gpio=noop())
+        self.device = max7219(self.serial, cascaded=1)
+        self.seg = sevensegment(self.device)
         
     def date(self): 
-        now   = datetime.now()
-        day   = now.day
-        month = now.month
-        year  = now.year - 2000
+        date(self.seg)
+        
+    def clock(self, seconds=10):
+        clock(self.seg, seconds)
+
+    def _show_message_vp(self, msg, delay=0.1):
+        show_message_vp(self.device, msg, delay=0.1)
     
-        mapping = [year % 10, 
-                   int(year / 10), 
-                   '-', 
-                   month % 10, 
-                   int(month / 10),  
-                   '-', 
-                   day % 10, 
-                   int(day / 10)
-                   ]
-        for n, m in enumerate(mapping): 
-            self.device.letter(self.deviceId, n + 1, m)
-        
-    def writeNumber(self, num): 
-        self.device.write_number(deviceId=self.deviceId, value=num, zeroPad=True)
-        
-    def brightness(self, intensity=16):
-        self.device.brightness(intensity)
-
-    def scrollRight(self): 
-        # Scroll Text
-        for i in range(8):
-            self.device.scroll_right()
-            time.sleep(0.1)
-        
-    def clear(self): 
-        self.device.clear()
-        
-if __name__ == '__main__': 
+    def _show_message_alt(self, msg, delay=0.1):
+        show_message_alt(self.device, msg, delay=0.1)
     
-    if len(sys.argv)>1: 
-        ss = sevensegments(0)
-        ss.writeNumber(sys.argv[1])
-    else: 
-        ss = sevensegments(0)
-        ss.date()
+    def message(self, msg, delay=0.1): 
+        self._show_message_vp(msg, delay)
 
+    def brightness(self,intensity=15):
+        "1 - ... - 15"
+        self.seg.device.contrast(intensity * 16)
+        
+#%%
 
-
+if __name__ == '__main__':
+    main()

e2840296610f26b885193b852893c342d6fa8eaf
Alexander Ignatov <ignalex@gmail.com>
2019-07-12 16:20:03 +1000

    ping iPhone

--- modules/PingIPhone.py
+++ modules/PingIPhone.py
@@ -158,8 +158,10 @@ def AcquireResult(allowed_delay_min = 5):
 
     process = Popen(com.split(' '), stdout=PIPE, stderr=PIPE)
     stdout, stderr = process.communicate()
+    
+    if type(stdout) != str : stdout = stdout.decode("utf-8") #for python3 > return is Byte like object
 
-    if ((datetime.now() + timedelta(seconds = 120)) - datetime.strptime(str(stdout.split('\t')[0]),'%Y-%m-%d %H:%M:%S')).seconds < allowed_delay_min * 60 + 120 :
+    if ((datetime.now() + timedelta(seconds = 120)) - datetime.strptime(stdout.split('\t')[0],'%Y-%m-%d %H:%M:%S')).seconds < allowed_delay_min * 60 + 120 :
         return stdout.replace('\n','').split('\t')[-1] == 'True'
     else:
         print ((datetime.now() - datetime.strptime(stdout.split('\t')[0],'%Y-%m-%d %H:%M:%S')).seconds)

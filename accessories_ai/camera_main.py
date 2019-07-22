60d1beeb12786c9221e849741e483c2195ea23cf
Alexander Ignatov <ignalex@gmail.com>
2019-07-22 19:24:42 +1000

    camera ip

--- accessories_ai/camera_main.py
+++ accessories_ai/camera_main.py
@@ -67,22 +67,15 @@ options = {
     },
     "srtp": True,
     "address": "192.168.1.7", #socket.gethostbyname(socket.gethostname()), #"192.168.1.7", 
-#    "start_stream_cmd" : ('ffmpeg -f video4linux2 -input_format h264 -video_size {width}x{height} '
-#        '-framerate 20 -i /dev/video0 '
-#        '-vcodec copy -an -payload_type 99 -ssrc 1 -f rtsp '
-#        '-b:v {v_max_bitrate}k -bufsize {v_max_bitrate}k '
-#        '-payload_type 99 -f rtp '
-#        '-srtp_out_suite AES_CM_128_HMAC_SHA1_80 -srtp_out_params {v_srtp_key} '
-#        'srtp://{address}:{v_port}?rtcpport={v_port}&'
-#        'localrtcpport={v_port}&pkt_size=1378')
-    "start_stream_cmd":  (
-      'ffmpeg -f video4linux2 -input_format h264 -i ' + DEV_VIDEO + ' -threads 4 '
-      '-vcodec copy -an -pix_fmt yuv420p -r {fps} '
-      '-b:v 2M -bufsize 2M '
-      '-payload_type 99 -ssrc {v_ssrc} -f rtp '
-      '-srtp_out_suite AES_CM_128_HMAC_SHA1_80 -srtp_out_params {v_srtp_key} '
-      'srtp://{address}:{v_port}?rtcpport={v_port}&'
-      'localrtcpport={v_port}&pkt_size=1316')    
+    "start_stream_cmd" : ('ffmpeg -f video4linux2 -input_format h264 -video_size {width}x{height} '
+        '-framerate 20 -i /dev/video0 '
+        '-vcodec copy -an -payload_type 99 -ssrc 1 -f rtsp '
+        '-b:v {v_max_bitrate}k -bufsize {v_max_bitrate}k '
+        '-payload_type 99 -f rtp '
+        '-srtp_out_suite AES_CM_128_HMAC_SHA1_80 -srtp_out_params {v_srtp_key} '
+        'srtp://{address}:{v_port}?rtcpport={v_port}&'
+        'localrtcpport={v_port}&pkt_size=1378')
+  
 }
 
 logger.debug('options: '+ str(options))

607aaac7e5f2195676604f36848cad2f9d77f743
Alexander Ignatov <ignalex@gmail.com>
2019-08-02 07:53:51 +1000

    pac to pa

--- modules/pa.py
+++ modules/pa.py
@@ -19,19 +19,26 @@ import __main__ as m
 import os,sys
 from common import CONFIGURATION, LOGGER
 
-def pa(arg):
-    'start PA.py on remote module using ssh'
+def pa(args):
+    """call common PA methods via api or local or on remote using ssh
+    - pa = direct|yes
+    - pa = ip|192.168.1.154,user|pi,ssh|/home/pi/.ssh/octopus,port|2227
+    - pa = api|,host|localhost,port|8083
+    """
 
     config = CONFIGURATION().pa.__dict__
-    config['arg'] =   arg[0] if type(arg) == list else arg
+    config['RUN'] = args[0] if type(args) == list else args # 1st param is MODULE
 
     if 'ssh' in config.keys():
-        cmd = "ssh -p {port} -i {ssh} {user}@{ip} nohup python /home/pi/git/pi/PA.py '{arg}'".format(**config)
+        cmd = "ssh -p {port} -i {ssh} {user}@{ip} nohup python /home/pi/git/pi/PA.py '{RUN}'".format(**config)
+    elif 'api' in config.keys():
+        config['params'] = '' if len(args) == 1 else '\&args='+';'.join(args[1:])
+        cmd = "curl {host}:{port}/cmnd?RUN={RUN}{params}".format(**config)
     else:
-        cmd = "python3 /home/pi/git/pi/PA.py '{arg}'".format(**config) #was &
+        cmd = "python3 /home/pi/git/pi/PA.py '{RUN}'".format(**config) #was &
     m.logger.info(cmd)
     os.system(cmd)
 
 if __name__ == '__main__':
     logger = LOGGER('pa')
-    pa(sys.argv[1:][0]) #pass 1st argument
+    pa([i for i in sys.argv[1:] if i != '-d']) #pass args as list
